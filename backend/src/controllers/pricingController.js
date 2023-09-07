c7b9459b01dabf4f3cd5af14eccde457201e34fa
Gustavo Menezes <gustavo.menezes@live.com>
2023-09-06 23:38:31 -0300

    style: Inclusao de comentários

--- backend/src/controllers/pricingController.js
+++ backend/src/controllers/pricingController.js
@@ -3,37 +3,35 @@ const csvParser = require("../utils/csvParser");
 const { query } = require("../config/database"); // Importe a função query corretamente
 
 function calculateNewPrice(productCode, newPrice, salesPrice, costPrice) {
-    // Aqui você pode implementar a lógica de cálculo do novo preço
-    // Por exemplo, você pode aplicar um desconto ou uma fórmula específica.
-    // Suponha que você aplique um desconto de 10%:
 
-    const percentVariable = 0.1;
+    //Compara o Novo Preço, com o preço de venda atual e o preõ de custo
+
+    // Percentual máximo de variação do preço 
+    let percentVariable = 0.1;
+
     const upperVariable = 1 + percentVariable;
     const downVariable = 1 - percentVariable;
 
-
-    if ( newPrice < costPrice ) {
+    // Verifica se o Novo Preço é maior que o preço de custo
+    if (newPrice < costPrice) {
         return {
             status: "error",
             reason: "Preço de venda inferior ao preço de custo",
             productCode: productCode,
             newPrice: newPrice,
-            oldPrice: salesPrice,
+            costPrice: costPrice
         };
-
-    } else if ( newPrice > salesPrice * upperVariable || newPrice < salesPrice * downVariable ) {
+        //Verifica se a variação do Novo Preço é maior que 10%
+    } else if ( newPrice > (salesPrice * upperVariable) || newPrice < (salesPrice * downVariable) ) {
         return {
             status: "error",
             reason: "Produto teve uma variação maior que 10%",
             productCode: productCode,
             newPrice: newPrice,
-            oldPrice: salesPrice,
-            costPrice: costPrice
+            oldPrice: salesPrice
         };
     }
 
-    // const currentPrice = existingProduct.price;
-    // const discountedPrice = currentPrice - (currentPrice * 0.1); // 10% de desconto
     return { status: "success", productCode: productCode, newPrice: newPrice };
 }
 
@@ -60,12 +58,12 @@ class PricingController {
                 const salesPrice = existingProduct[0]["sales_price"];
                 const costPrice = existingProduct[0]["cost_price"];
 
+                // Pular para o próximo produto se não for encontrado
                 if (!existingProduct.length) {
                     console.error("Produto não encontrado no banco de dados.");
-                    continue; // Pular para o próximo produto se não for encontrado
+                    continue; 
                 }
-
-
+                // Verifica se o preço se adequa a regra de negócio
                 const resultCalculatedNewPrice = calculateNewPrice(
                     productCode,
                     newPrice,
@@ -73,47 +71,26 @@ class PricingController {
                     costPrice
                 );
 
-                // console.log(
-                //     "Product.log " +
-                //         JSON.stringify(resultCalculatedNewPrice.status)
-                // );
-
-
                 // Atualizar o banco de dados com o novo preço
                 switch (resultCalculatedNewPrice.status) {
                     case "error":
-                        // console.log("Preço do produto " + productCode + " não atualizado. Motivo: " + resultCalculatedNewPrice.reason);
+
                         productsError.push(resultCalculatedNewPrice);
                         break;
-                        case "success":
-                            const updateSql =
+                    case "success":
+                        const updateSql =
                             "UPDATE products SET sales_price = ? WHERE code = ?";
-                            await query(updateSql, [
-                                product.new_price,
-                                product.product_code,
-                            ]);
-                            productsSuccess.push(resultCalculatedNewPrice);
-                            // console.log("Produto " + productCode + " atualizou o preço para " + newPrice)
-                            
-                        }
-                        
-                        
-                        // if (resultCalculatedNewPrice.status != "error") {
-                //     const updateSql =
-                //         "UPDATE products SET sales_price = ? WHERE code = ?";
-                //     await query(updateSql, [
-                //         product.new_price,
-                //         product.product_code,
-                //     ]);
-                //     console.log("Produto " + productCode + " atualizou o preço para " + newPrice)
-                // } else {
-                    //     console.log("Preço do produto " + productCode + " não atualizado. Motivo: " + resultCalculatedNewPrice.reason)
-                    // }
+                        await query(updateSql, [
+                            product.new_price,
+                            product.product_code,
+                        ]);
+                        productsSuccess.push(resultCalculatedNewPrice);
                 }
-                console.log(productsError)
-                console.log("----------------------------------------------------")
-                console.log(productsSuccess)
-                
+            }
+            console.log(productsError);
+            console.log("----------------------------------------------------");
+            console.log(productsSuccess);
+
             res.status(200).json({ message: "Preços atualizados com sucesso" });
         } catch (error) {
             console.error("Erro na validação/processamento do arquivo:", error);
